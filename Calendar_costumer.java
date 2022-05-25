2c7b04efe6965b55e079f58fb2a8a8d36c9adcf1
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-05-25 15:42:32 +0300

    Add files via upload

---
+++ Calendar_costumer.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+import java.time.LocalTime;
+public class Calendar_costumer
+{
+    public customer customer;
+    public Date date;
+    public LocalTime time;
+    public Calendar_costumer(customer customer,Date date,LocalTime time)
+    {
+       this.customer=customer;
+       this.date=date;
+       this.time=time;
+    }
+
+    
+}
---
+++ Calendar_owner.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+import java.time.LocalTime;
+public class Calendar_owner
+{
+    public owner owner;
+    public Date date;
+    public LocalTime time;
+    public Calendar_owner(owner owner,Date date,LocalTime time)
+    {
+       this.owner=owner;
+       this.date=date;
+       this.time=time;
+    }
+
+    
+}
