b34ba819c493c92ed4be8545324bb6290b00a74f
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-05-28 21:25:50 +0300

    Update moderation.java

--- moderation.java
+++ moderation.java
@@ -1,19 +1,12 @@
 public class moderation {
-    public int flagged_posts;
-    public int user_id_flag;
-    public String owner_tickets;
-    public String customer_tickets;
-    public int flagged_drones;
-    public int customer_id_flag;
-    public String user_id_ticket;
+    public posts posts;
+    public tickets tickets;
+    public drone drone;
     
-    public moderation(int flagged_posts, int user_id_flag, String owner_tickets, String customer_tickets, int flagged_drones, int customer_id_flag, String user_id_ticket) {
-        this.flagged_posts=flagged_posts;
-        this.user_id_flag=user_id_flag;
-        this.owner_tickets=owner_tickets;
-        this.customer_tickets=customer_tickets;
-        this.flagged_drones=flagged_drones;
-        this.customer_id_flag=customer_id_flag;
-        this.user_id_ticket=user_id_ticket;
+    
+    public moderation(posts posts,tickets tickets, drone drone) {
+        this.posts=posts;
+        this.tickets=tickets;
+        this.drone=drone;
     }
 }
