00f8cb64d4e19c0bbe2d6c307ac68d208056d5be
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-05-28 21:26:33 +0300

    Update history.java

--- history.java
+++ history.java
@@ -1,26 +1,27 @@
+import java.util.Date;
+import java.time.LocalTime;
 public class history
 {
-  public int drone_id;
+  public drone drone;
   public String location;
-  public int costumer_id;
-  public String date_and_time;
-  public int owner_id;
+  public Customer customer;
+  public Date date;
+  public LocalTime time;
+  public Owner owner;
   public int flight_id;
   public int photo_id;
   public int payment_id;
   public String flight_path;
   
-  public history(  int drone_id,String location,int costumer_id,String date_and_time,int owner_id,int flight_id,int photo_id,int payment_id,String flight_path){
-     this.drone_id=drone_id;
+  public history( drone drone,String location,Customer customer,Date date, LocalTime time,Owner owner,int flight_id,int photo_id,int payment_id,String flight_path){
+     this.drone=drone;
      this.location=location;
-     this.owner_id=owner_id;
+     this.owner=owner;
      this.payment_id=payment_id;
      this.photo_id=photo_id;
-     this.costumer_id=costumer_id;
-     this.date_and_time=date_and_time;
+     this.customer=customer;
+     this.date=date;
+     this.time=time;
      this.flight_id=flight_id;
      this.flight_path=flight_path;
     }
-  
-  
-}
