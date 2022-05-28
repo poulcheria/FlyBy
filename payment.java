9ca20e3ee337d7e1b072fd37f1a23875081539e8
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-05-28 21:24:48 +0300

    Update payment.java

--- payment.java
+++ payment.java
@@ -1,26 +1,27 @@
 
+import java.util.Date;
+import java.time.LocalTime;
 public class payment
 {
-  public int drone_id;
+  public rent rent;
   public String location;
-  public int costumer_id;
-  public String date_and_time;
-  public int owner_id;
+  public Date date;
+  public LocalTime time;
   public int payment_id;
   public float price;
   public String payment_method;
   
-  public payment(int drone_id,String location,float price,String payment_method,int owner_id,int payment_id,String date_and_time,int costumer_id){
-     this.drone_id=drone_id;
+  public payment(rent rent,String location,float price,String payment_method,int payment_id,Date date,LocalTime time){
+     this.rent=rent;
      this.location=location;
-     this.owner_id=owner_id;
+     
      this.payment_id=payment_id;
-     this.costumer_id=costumer_id;
-     this.date_and_time=date_and_time;
+     
+     this.date=date;
+     this.time=time;
      this.price=price;
      this.payment_method=payment_method;
     }
   
   
 }
-
