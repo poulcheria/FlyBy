ea5ff32fb11bc0e49b1313ce0b476a9342de1b48
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-05-28 21:30:04 +0300

    Update user.java

--- user.java
+++ user.java
@@ -1,10 +1,10 @@
 
 
-public class user {
+public class User {
     public static int user_id;
     public static String user_type;
     
-    public user (int user_id, String user_type)
+    public User (int user_id, String user_type)
     {
         this.user_id=user_id;
         this.user_type=user_type;
