fc8de744b71f67990a192933fd5acae9b0a3c817
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-05-28 21:23:04 +0300

    Update photo_gallery.java

--- photo_gallery.java
+++ photo_gallery.java
@@ -2,14 +2,12 @@ import javax.imageio.ImageIO;
 public class photo_gallery {
     public int photo_id;
     public String imagePath;
-    public int user_id;
-    public int rent_id;
+    public rent rent;
 
-    public photo_gallery(int photo_id, String imagePath, int user_id, int rent_id) {
-
-    }
+    public photo_gallery(int photo_id, String imagePath, rent rent) {
+        this.rent=rent;
         this.photo_id=photo_id;
         this.imagePath=imagePath;
-        this.user_id=user_id;
-        this.rent_id=rent_id;
+        
+    }
 }
