ce40efce8942c593545285f7f203f4718ea7d44e
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-05-30 22:59:11 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,19 @@
-import java.util.*; 
-import java.lang.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Date;
 public class Main
 {
     
     
     public static void main(String[]args){
         Date birthdate= new Date();
-        drone newdrone= new drone(0 ,"." ,0,"." ,0 ,"." ,0,0, 0);
+        drone newdrone= new drone(0 ,"." ,0,"." ,0 ,"." ,0,0, 0,1);
         Customer customer= new Customer(1,"custormer","niki" ,"portopass","nikiporto@gmail.com",2000,birthdate,"card" ,"C:UsersnikiPicturesSaved Pictures");
         Owner owner= new Owner(2,"Owner","poul" ,"poulpass"  ,"poulz@gmail.com",10000,birthdate,"crypto" ,"C:UserspoulcheriaPicturesSaved Pictures",newdrone);
         Scanner customer_owner = new Scanner(System.in);
         System.out.println("Hello, please choose your user type, press 1 for customer and 0 for owner\n");
         int Usertype= customer_owner.nextInt();
+        rent nrent=new rent(1,customer,owner,newdrone);
+        rating newrating= new rating(".",nrent,0 );
+        customer.Rent_a_drone(customer);
+        //newrating.comment_rating();
         if(Usertype==1 ){
             customer.customer_Sign_Up();
         }
