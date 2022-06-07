3e5f3c7cb65a57381b1c41b39df6d1d223748da4
nraptisss <104900174+nraptisss@users.noreply.github.com>
2022-06-07 19:00:46 +0300

    Update payment.java

--- payment.java
+++ payment.java
@@ -22,6 +22,29 @@ public class payment
      this.price=price;
      this.payment_method=payment_method;
     }
-  
+  public void payment(){
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Choose payment method:");
+    System.out.println("1. Bank Card");
+    System.out.println("2. Cryptocurrency");
+
+    Scanner method= new Scanner(System.in);   
+    int imethod = method.nextInt();
+
+    if ( imethod == 1 ) {
+        System.out.println()
+        //send OTP
+        //validate payment
+        System.out.println("You successfully rented drone" + this.drone + ", your flight is scheduled for " + this.date + this.time + "and your total payment is " + this.price);
+        System.out.println("Your payment number is " + this.payment_id);
+        }
+    else if ( imethod == 2 ) {
+        //Create deposit adress
+        System.out.println("You successfully rented drone" + this.drone + ", your flight is scheduled for " + this.date + this.time + "and your total payment is " + this.price);
+        System.out.println("Your transaction ID  is " + this.payment_id);
+        }
+    }
+}
   
 }
