67de02832f7f9f7cc527971f6786bb7c3d75fc04
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-06-11 22:24:04 +0300

    Update rent.java

--- rent.java
+++ rent.java
@@ -2,14 +2,12 @@ public class rent
 {
   public int rent_id;
   public Customer customer;
-  public Owner owner;
   public drone drone;
   
   
-  public rent(int rent_id ,Customer customer,Owner owner,drone drone){
+  public rent(int rent_id ,Customer customer,drone drone){
         this.rent_id=rent_id;
         this.customer=customer;
-        this.owner=owner;
         this.drone=drone;
     }
 }
