cc3b349b4c77fd2fe1b5adae76035e0abc1b8018
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-06-11 22:15:18 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,35 @@
+import java.util.*; 
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Date;
 public class Main
 {
     
     
     public static void main(String[]args){
         Date birthdate= new Date();
-        drone newdrone= new drone(0 ,"." ,0,"." ,0 ,"." ,0,0, 0,1);
-        Customer customer= new Customer(1,"custormer","niki" ,"portopass","nikiporto@gmail.com",2000,birthdate,"card" ,"C:UsersnikiPicturesSaved Pictures");
-        Owner owner= new Owner(2,"Owner","poul" ,"poulpass"  ,"poulz@gmail.com",10000,birthdate,"crypto" ,"C:UserspoulcheriaPicturesSaved Pictures",newdrone);
+    
+        
+        admin admin = new admin(10,"admin" ,"flyby@gmail.com" , "123" , birthdate);
+ 
         Scanner customer_owner = new Scanner(System.in);
-        System.out.println("Hello, please choose your user type, press 1 for customer and 0 for owner\n");
+        
+        System.out.println("Hello, please choose your user type, press 1 for customer and 2 for owner and 0 for admin\n");
         int Usertype= customer_owner.nextInt();
-        rent nrent=new rent(1,customer,owner,newdrone);
-        rating newrating= new rating(".",nrent,0 );
-        customer.Rent_a_drone(customer);
-        //newrating.comment_rating();
+        Customer customer= new Customer(1,"custormer","anon" ,"anon","anon",0,birthdate,"anon" ,"anon");
+        Owner owner= new Owner(1,"Owner","anon" ,"anon","anon",0,birthdate,"anon" ,"anon",null,1);
         if(Usertype==1 ){
-            customer.customer_Sign_Up();
+            customer.customer_interface(customer);
+        }
+        else if(Usertype==2 ){
+            owner.owner_interface(owner);
         }
         else{
-            owner.owner_Sign_Up();
+        admin.admin_login();
         }
+        
         
     }
 
