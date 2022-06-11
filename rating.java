257aa5826287410f9fc8285bfb4d58385c685b15
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-06-11 22:23:43 +0300

    Update rating.java

--- rating.java
+++ rating.java
@@ -16,30 +16,6 @@ public class rating
      this.commentList=new ArrayList<rating>();
     }
     
-  public void comment_rating() {
-       Scanner rcrentid = new Scanner(System.in);
-       System.out.println("please add the rent id\n");
-       String ratingrentid= rcrentid.nextLine();
-       int ratingrentidint = Integer.parseInt(ratingrentid);
-       
-       Scanner newComment = new Scanner(System.in);
-       System.out.println("Type your comment\n");
-       String nComment= newComment.nextLine();
-       
-       Scanner newRating = new Scanner(System.in);
-       System.out.println("Enter your rating 1-5\n");
-       String nRating= newRating.nextLine();
-       float fnRating=Float.parseFloat(nRating);  
-       
-       Date birthdate=new Date();
-       drone newdrone=new drone(0,".",0,".",0,".",0,0,0,1,1,1);
-       Customer newcustomer=new Customer(0,"costumer","name","pass","email",687,birthdate,"card","filepath");
-       Owner newowner= new Owner(55,"Owner","name","nOpass" ,"nOemail",878,birthdate,"nOpay" ,"nOimg",newdrone,1);
-       rent nrent=new rent(ratingrentidint,newcustomer,newowner,newdrone);
-       
-       rating newrating= new rating(nComment,nrent,fnRating );
-       
-       this.commentList.add(newrating);
-    }
+  
   
 }
