c4d9b15f2ed9d6a388a5e04bbf8f8f11c58ca1ba
poulcheria <66693984+poulcheria@users.noreply.github.com>
2022-06-12 08:55:04 +0300

    Add files via upload

---
+++ FlybyException.java
@@ -0,0 +1,15 @@
+public class FlybyException extends Exception
+{
+  
+
+    public FlybyException(){}
+    public FlybyException(String exc){
+    super(exc);
+    System.out.println(exc);
+    }
+    
+        
+    
+
+  
+}
